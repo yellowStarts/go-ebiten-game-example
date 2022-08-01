439028e8d3dd1747f9394d19e5d5041b3587d11b
huanggz <918031078@qq.com>
2022-08-01 15:34:23 +0800

    Add background in screen

--- main.go
+++ main.go
@@ -7,16 +7,34 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var (
+	err        error
+	background *ebiten.Image
+)
+
+// init 初始化
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// update 更新画布
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello World!")
+	op := &ebiten.DrawImageOptions{}
+	// 屏幕起始坐标
+	op.GeoM.Translate(0, 0)
+	screen.DrawImage(background, op)
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello World!"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "Hello World!"); err != nil {
 		log.Fatal(err)
 	}
 }
